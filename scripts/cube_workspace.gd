df8b6dda53601386ccc0c33e1549ebb0707dfea1
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-08-31 10:26:40 +0200

    setup workspace with obtained cubeing data

--- scripts/cube_workspace.gd
+++ scripts/cube_workspace.gd
@@ -2,16 +2,42 @@ extends Control
 
 
 const CUBE_CONTROL = preload("res://scenes/cube_control.tscn")
+const FILEPATH: String = "res://data/kobozo.txt"
+const STARTING_DIAMETER: int = 15
+
+
+@export var woodtype: String = "bükk"
+@export var woodlength: float = 3.2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    for i in range(15):
+    var cube_data: Array = get_cubedata(FILEPATH, woodtype, woodlength)
+    for i in range(cube_data.size()):
         var cube_control = CUBE_CONTROL.instantiate()
         $ScrollContainer/VBoxContainer.add_child(cube_control)
-        cube_control.set_cube_data(15 + i, 20 + i)
+        cube_control.set_cube_data(STARTING_DIAMETER + i, cube_data[i])
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-    pass
+func get_cubedata(file_path: String, woodtype_: String, woodlength_: float) -> Array:
+    var file = FileAccess.open(file_path, FileAccess.READ)
+    var result = []  
+    var line: String 
+    
+    if file:
+        # find the wood type
+        while file.get_position() < file.get_length():
+            line = file.get_line()
+            if line.begins_with(woodtype_):
+                break
+        # find the length        
+        while file.get_position() < file.get_length():
+            line = file.get_line()
+            if line.begins_with(" " + str(woodlength_)):
+                break
+        # read the cube data
+        var cubedata = line.split(":")[1]
+        for number in cubedata.strip_edges().split(" "):
+            result.append(int(number))
+            
+    return result
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,35 +4,9 @@ extends Node2D
 const CUBE_WORKSPACE = preload("res://scenes/cube_workspace.tscn")
 
 
-func _ready() -> void:
-    var file_path = "res://data/kobozo.txt"
-    
+func _ready() -> void:    
     var cube_workspace = CUBE_WORKSPACE.instantiate()
+    cube_workspace.woodtype = "tölgy"
+    cube_workspace.woodlength = 3.1
     add_child(cube_workspace)
     cube_workspace.set_position(Vector2(0, 0))
-    
-    for data in get_cubedata(file_path, "tölgy", 3.1):
-        print(data)
-
-
-func get_cubedata(file_path: String, woodtype: String, length: float) -> Array:
-    var file = FileAccess.open(file_path, FileAccess.READ)
-    var result = []  
-    var line: String 
-    
-    if file:
-        # find the wood type
-        while file.get_position() < file.get_length():
-            line = file.get_line()
-            if line.begins_with(woodtype):
-                break
-        # find the length        
-        while file.get_position() < file.get_length():
-            line = file.get_line()
-            if line.begins_with(" " + str(length)):
-                break
-        # read the cube data
-        var cubedata = line.split(":")[1]
-        for number in cubedata.strip_edges().split(" "):
-            result.append(str(number))
-    return result
