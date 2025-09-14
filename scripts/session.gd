07928feb12fab6acd8b1d0bcc263f744be6f23fa
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-09-14 16:34:44 +0200

    create session script

---
+++ scripts/session.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var company: String = "Nyirma Ebt."
+var city: String = "Herend"
+var site: String = "26B"
+var type: String = "tölgy"
+var length: float = 2.0
+var cube_data: Array = [123, 456, 789]
+var cube_count: Array = [1, 2, 3]
