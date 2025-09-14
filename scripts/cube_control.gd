0d06766f900599892e309a9be376336eb0394fc2
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-09-14 15:31:14 +0200

    display total volume

--- scripts/cube_control.gd
+++ scripts/cube_control.gd
@@ -1,26 +1,19 @@
 extends Control
 
-
-const PRECISION: float = 0.01
-
-
+const PRECISION: float = 0.001
 var counter: int
 var diameter: int  # diamater is represented in centimetres
 var cube: int  # cube data is represented in m3, must be multiplied by the given precision
-
 
 func _ready() -> void:
     counter = 0
     set_cube_data()
 
-
 func _on_decrease_button_pressed() -> void:
     counter -= 1
 
-
 func _on_increase_button_pressed() -> void:
     counter += 1
-
 
 func _process(_delta: float) -> void:
     var decrease_button: Button = $HBoxContainer/DecreaseButton
@@ -35,9 +28,11 @@ func _process(_delta: float) -> void:
     counter_label.text = str(counter)
     diameter_label.text = str(diameter)
     cubic_label.text = str(cube)
-    volume_label.text = "%.2f" % (cube * counter * PRECISION)
-
+    volume_label.text = "%.2f" % get_volume()
 
 func set_cube_data(diameter_: int = 20, cube_: int = 15) -> void:
     diameter = diameter_
     cube = cube_
+
+func get_volume() -> float:
+    return cube * counter * PRECISION
--- scripts/cube_workspace.gd
+++ scripts/cube_workspace.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
-
 const CUBE_CONTROL = preload("res://scenes/cube_control.tscn")
 const STARTING_DIAMETER: int = 12
-
 
 @export var woodtype: String = "tölgy"
 @export var woodlength: float = 2.0
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     var cube_data: Array = File_IO.get_cubedata(woodtype, woodlength)
-    print(cube_data.size())
     for i in range(cube_data.size()):
         var cube_control = CUBE_CONTROL.instantiate()
         $ScrollContainer/VBoxContainer.add_child(cube_control)
         cube_control.set_cube_data(STARTING_DIAMETER + i, cube_data[i])
+        
+func _process(_delta: float) -> void:
+    $SubtitleLabel.text = "Farakás: %.3f m3" % get_total_volume()
+
+func get_total_volume() -> float:
+    var total: float = 0.0
+    for single in $ScrollContainer/VBoxContainer.get_children():
+        total += single.get_volume()
+    return total
