e5ad66a8e342c6dd6ce8777d70a42944b821087e
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-10-08 19:57:29 +0200

    restore persisted session

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
     scene = RESTORE_SCENE.instantiate()
     add_child(scene)
     scene.start_new_session.connect(_on_new_session_started)
+    scene.restore_session.connect(_on_continue_session)
 
 func _on_new_session_started() -> void:
     remove_prev_scene()
@@ -46,6 +47,14 @@ func _on_cube_done() -> void:
     remove_prev_scene()
     scene = REPORT_SCENE.instantiate()
     add_child(scene)
+
+func _on_continue_session(session: Dictionary) -> void:
+    remove_prev_scene()
+    scene = WORKSPACE_SCENE.instantiate()
+    CurrentPile.set_session_data(session)
+    add_child(scene)
+    scene.done.connect(_on_cube_done)
+    scene.set_position(Vector2(0, 0))
 
 func remove_prev_scene() -> void:
     remove_child(scene)
--- scripts/restore.gd
+++ scripts/restore.gd
@@ -3,6 +3,7 @@ extends Node2D
 const RESTORE_CONTROL = preload("res://scenes/restore_control.tscn")
 
 signal start_new_session
+signal restore_session
 
 func _ready() -> void:
     var sessions = File_IO.get_sessions()
@@ -11,6 +12,7 @@ func _ready() -> void:
         pile.set_session_data(session)
         var restore_control = RESTORE_CONTROL.instantiate()
         $ScrollContainer/VBoxContainer.add_child(restore_control)
+        restore_control.continue_session.connect(_on_continue_session_button_pressed.bind(session))
         restore_control.populate(pile)        
 
 func _process(_delta: float) -> void:
@@ -18,3 +20,6 @@ func _process(_delta: float) -> void:
 
 func _on_new_session_button_pressed() -> void:
     start_new_session.emit()
+
+func _on_continue_session_button_pressed(session: Dictionary) -> void:
+    restore_session.emit(session)
