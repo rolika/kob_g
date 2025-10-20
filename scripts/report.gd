4b2709c74e312812a56aa78856e316987978be8e
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-10-20 19:57:02 +0200

    new report display

--- scripts/report.gd
+++ scripts/report.gd
@@ -1,7 +1,5 @@
 extends ColorRect
 
-const HEADER: String = "%s, %s, rakodóhely: %s\n%s hosszú %s rönkök, összesen: %s"
-const LINE: String = "átmérő: %d cm; %d db x %s m3/db = %s m3"
 const DATE_FMT: String = "%s.%s.%s."
 const DATE: String = "%s, %s"
 
@@ -10,13 +8,44 @@ func _ready() -> void:
     cellstyle.bg_color = Color(0, 0, 0, 0.0)
     cellstyle.border_color = Color(0, 0, 0)
     cellstyle.set_border_width_all(1)
-    $HeaderLabel.text = HEADER % [
-        CurrentPile.company,
-        CurrentPile.city, 
-        CurrentPile.site, 
-        CurrentPile.get_length_fmt(), 
-        CurrentPile.type, 
-        CurrentPile.get_total_volume_fmt()]
+    var company_label: Label = Label.new()
+    var company_name: Label = Label.new()
+    company_name.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+    var city_label: Label = Label.new()
+    var city_name: Label = Label.new()
+    var site_label: Label = Label.new()
+    var site_name: Label = Label.new()
+    var type_label: Label = Label.new()
+    var type_name: Label = Label.new()
+    type_name.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+    var length_label: Label = Label.new()
+    var length_value: Label = Label.new()
+    var volume_label: Label = Label.new()
+    volume_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+    var volume_value: Label = Label.new()
+    volume_value.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+    company_label.text = "erdőbirtokosság:"
+    company_name.text = CurrentPile.company
+    city_label.text = "helység:"
+    city_name.text = CurrentPile.city
+    site_label.text = "rakodóhely:"
+    site_name.text = CurrentPile.site
+    type_label.text = "fafajta:"
+    type_name.text = CurrentPile.type
+    length_label.text = "rönkhossz:"
+    length_value.text = CurrentPile.get_length_fmt()
+    volume_label.text = "összesen:"
+    volume_value.text = CurrentPile.get_total_volume_fmt()
+    $HeadGrid1.add_child(company_label)
+    $HeadGrid1.add_child(company_name)
+    $HeadGrid1.add_child(city_label)
+    $HeadGrid1.add_child(city_name)
+    $HeadGrid2.add_child(site_label)
+    $HeadGrid2.add_child(site_name)
+    $HeadGrid2.add_child(type_label)
+    $HeadGrid2.add_child(type_name)
+    $HeadGrid2.add_child(length_label)
+    $HeadGrid2.add_child(length_value)
     var index = 0
     for cube in CurrentPile.counter:
         var diameter: Label = Label.new()
@@ -45,6 +74,14 @@ func _ready() -> void:
         $ReportGrid.add_child(singlevolume)
         $ReportGrid.add_child(volume)
         index += 1
+    var spacer1: Label = Label.new()
+    var spacer2: Label = Label.new()
+    spacer1.text = ""
+    spacer2.text = ""
+    $ReportGrid.add_child(volume_label)
+    $ReportGrid.add_child(spacer1)
+    $ReportGrid.add_child(spacer2)
+    $ReportGrid.add_child(volume_value)
     var today = Time.get_date_dict_from_system()
     var date_fmt = DATE_FMT % [today.year, today.month, today.day]
     $DateLabel.text = DATE % [CurrentPile.city, date_fmt]
