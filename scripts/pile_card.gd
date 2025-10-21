1d60a5126e05d671aee485cfe6d5842b795a0ef6
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-10-21 19:53:24 +0200

    sanitize pile card

--- scripts/pile_card.gd
+++ scripts/pile_card.gd
@@ -6,12 +6,12 @@ func _init(pile_arg: Pile = CurrentPile) -> void:
     pile = pile_arg
 
 func _process(_delta: float) -> void:
-    $CompanyLabel.text = pile.company
-    $CityLabel.text = pile.city
-    $SiteLabel.text = pile.site
-    $TypeLabel.text = pile.type
-    $LengthLabel.text = pile.get_length_fmt()
-    $VolumeLabel.text = pile.get_total_volume_fmt()
+    $GridContainer/CompanyLabel.text = pile.company
+    $GridContainer/CityLabel.text = pile.city
+    $GridContainer/SiteLabel.text = pile.site
+    $GridContainer/TypeLabel.text = pile.type
+    $GridContainer/LengthLabel.text = pile.get_length_fmt()
+    $GridContainer/VolumeLabel.text = pile.get_total_volume_fmt()
 
 func set_pile(pile_arg: Pile) -> void:
     pile = pile_arg
