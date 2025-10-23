a362b2a669bcd8265dbf1a77dbac26e388de8b71
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-10-23 10:37:14 +0200

    add pine icon for pine woods

--- scripts/type_options.gd
+++ scripts/type_options.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 const ICON: CompressedTexture2D = preload("res://assets/icons/tree_icon.png")
+const PINE_ICON: CompressedTexture2D = preload("res://assets/icons/pine_icon.png")
 signal type_selected(type: String)
 
 func _ready() -> void:
     for woodtype in File_IO.get_woodtypes():
         var button = Button.new()
         button.text = woodtype
-        button.icon = ICON
+        if "fenyő" in woodtype:
+            button.icon = PINE_ICON
+        else:
+            button.icon = ICON
         button.vertical_icon_alignment = VERTICAL_ALIGNMENT_TOP
         button.icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
         button.custom_minimum_size = Vector2(240, 200)
