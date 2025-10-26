73e7448e2e105ba6fa3235b67062df58906ffd3e
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-10-26 17:44:45 +0100

    route focus when submitting the lineedit

--- scripts/session.gd
+++ scripts/session.gd
@@ -8,14 +8,30 @@ func _ready() -> void:
 func _on_company_line_edit_text_changed(new_text: String) -> void:
     CurrentPile.company = new_text
 
+func _on_company_line_edit_text_submitted(new_text: String) -> void:
+    CurrentPile.company = new_text
+    $CityLineEdit.call_deferred("grab_focus")
+
 func _on_city_line_edit_text_changed(new_text: String) -> void:
     CurrentPile.city = new_text
+
+func _on_city_line_edit_text_submitted(new_text: String) -> void:
+    CurrentPile.city = new_text
+    $SiteLineEdit.call_deferred("grab_focus")
 
 func _on_site_line_edit_text_changed(new_text: String) -> void:
     CurrentPile.site = new_text
 
+func _on_site_line_edit_text_submitted(new_text: String) -> void:
+    CurrentPile.site = new_text
+    $PersonLineEdit.call_deferred("grab_focus")
+
 func _on_person_line_edit_text_changed(new_text: String) -> void:
     CurrentPile.person = new_text
+
+func _on_person_line_edit_text_submitted(new_text: String) -> void:
+    CurrentPile.person = new_text
+    $SubmitButton.call_deferred("grab_focus")
 
 func _process(_delta: float) -> void:
     if CurrentPile.is_valid():
