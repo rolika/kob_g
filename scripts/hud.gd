746d618c2acd9b3833c294dbc062207d1caeb12f
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-11-11 19:30:45 +0100

    add script skeleton to hud

---
+++ scripts/hud.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+signal backward
+signal forward
+signal home
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+    pass
+
+func _on_backward_button_pressed() -> void:
+    backward.emit()
+
+func _on_forward_button_pressed() -> void:
+    forward.emit()
+
+func _on_home_button_pressed() -> void:
+    home.emit()
