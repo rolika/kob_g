1ee138c0571bf5f45a2ad87122f9922b442ce542
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-11-17 19:16:15 +0100

    cubing is done in hud

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,30 +13,42 @@ var scene: Node = null
 func _ready() -> void:
     scene = RESTORE_SCENE.instantiate()
     hud.add(scene)
+    hud.header_backward_button.disabled = true
+    hud.footer_backward_button.disabled = true
     hud.forward.connect(_on_new_session_started)
     scene.restore_session.connect(_on_show_session)
 
 func _on_new_session_started() -> void:
+    hud.forward.disconnect(_on_new_session_started)
     scene = SESSION_SCENE.instantiate()    
     hud.add(scene)
-    hud.forward.disconnect(_on_new_session_started)
     hud.header_forward_button.disabled = true
     hud.footer_forward_button.disabled = true
     hud.forward.connect(_on_session_submitted)    
     scene.check.connect(_on_check)
     
 func _on_session_submitted() -> void:
+    hud.forward.disconnect(_on_session_submitted)    
+    hud.header_forward_button.disabled = true
+    hud.footer_forward_button.disabled = true
     scene = TYPE_SCENE.instantiate()
     hud.add(scene)
     scene.type_selected.connect(_on_type_selected)
 
 func _on_type_selected(type: String) -> void:
+    hud.header_forward_button.disabled = true
+    hud.footer_forward_button.disabled = true
     scene = LENGTH_SCENE.instantiate()
     CurrentPile.type = type
     hud.add(scene)
     scene.length_selected.connect(_on_length_selected)
 
 func _on_length_selected(length: float) -> void:
+    hud.header_backward_button.disabled = true
+    hud.footer_backward_button.disabled = true
+    hud.header_forward_button.disabled = false
+    hud.footer_forward_button.disabled = false
+    hud.forward.connect(_on_cube_done)
     scene = WORKSPACE_SCENE.instantiate()
     CurrentPile.length = length
     CurrentPile.init()
@@ -44,28 +56,36 @@ func _on_length_selected(length: float) -> void:
     scene.set_position(Vector2(0, 0))
 
 func _on_cube_done() -> void:
-    remove_prev_scene()
+    hud.header_forward_button.disabled = false
+    hud.footer_forward_button.disabled = false
+    hud.header_backward_button.disabled = false
+    hud.footer_backward_button.disabled = false
+    hud.forward.disconnect(_on_cube_done)
+    hud.forward.connect(_ready)
+    hud.backward.connect(_ready)
     scene = REPORT_SCENE.instantiate()
-    add_child(scene)
-    scene.back.connect(_ready)
+    hud.add(scene)
 
 func _on_show_session(session: Dictionary) -> void:
-    remove_prev_scene()
+    hud.header_forward_button.disabled = false
+    hud.footer_forward_button.disabled = false
+    hud.header_backward_button.disabled = false
+    hud.footer_backward_button.disabled = false
+    hud.forward.connect(_on_continue_session)
     scene = SESSION_SCENE.instantiate()
     CurrentPile.set_session_data(session)
-    add_child(scene)
+    hud.add(scene)
     scene.set_session()
-    scene.submit.connect(_on_continue_session)
 
-func remove_prev_scene() -> void:
-    remove_child(scene)
-    scene.queue_free()
-
-func _on_continue_session() -> void:    
-    remove_prev_scene()
+func _on_continue_session() -> void:
+    hud.header_backward_button.disabled = true
+    hud.footer_backward_button.disabled = true
+    hud.header_forward_button.disabled = false
+    hud.footer_forward_button.disabled = false
+    hud.forward.disconnect(_on_continue_session)
+    hud.forward.connect(_on_cube_done)
     scene = WORKSPACE_SCENE.instantiate()
-    add_child(scene)
-    scene.done.connect(_on_cube_done)
+    hud.add(scene)
 
 func _on_check() -> void:
     if CurrentPile.is_valid():
