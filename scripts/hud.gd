daf9a4f4e1b24133fbca9dd86216243f88811b5e
Weisz Roland <weisz.roland@pohlen-dach.hu>
2025-11-29 17:48:19 +0100

    add & use specific functions to controll hud ui

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+class_name Hud extends MarginContainer
 
 signal backward
 signal forward
@@ -20,8 +20,40 @@ func _on_forward_button_pressed() -> void:
 func _on_home_button_pressed() -> void:
     home.emit()
 
-func add(content: Container) -> void:
+func change_screen(content: Container) -> void:
+    disable_all()
+    disconnect_all()
     if screen != null:
         screen.call_deferred("free")
     screen = content
     $VBoxContainer/ContentContainer.add_child(screen)
+
+func disconnect_all() -> void:
+    for connection in forward.get_connections():
+        forward.disconnect(connection.callable)
+    for connection in backward.get_connections():
+        backward.disconnect(connection.callable)
+
+func disable_forward() -> void:
+    header_forward_button.disabled = true
+    footer_forward_button.disabled = true
+
+func disable_backward() -> void:
+    header_backward_button.disabled = true
+    footer_backward_button.disabled = true
+
+func disable_all() -> void:
+    disable_forward()
+    disable_backward()
+
+func enable_forward() -> void:
+    header_forward_button.disabled = false
+    footer_forward_button.disabled = false
+
+func enable_backward() -> void:
+    header_backward_button.disabled = false
+    footer_backward_button.disabled = false
+
+func enable_all() -> void:
+    enable_forward()
+    enable_backward()
